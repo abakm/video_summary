bdd213e154209a3fe891c6fc0049543ec3f7094c
abakm <akhilbalakrishnanakm@gmail.com>
2025-09-23 23:08:33 +0530

    Video capture changes

--- graph/key_moments.py
+++ graph/key_moments.py
@@ -1,6 +1,6 @@
+import os
 import cv2
 import numpy as np
-
 from gdown import download as GoogleDriveDownload
 from yt_dlp import YoutubeDL
 
@@ -26,18 +26,39 @@ class KeyMoments:
     @staticmethod
     def detect_key_moments():
         stream_url = KeyMoments.get_youtube_stream()
+        print("STREAM URL: ", stream_url)
 
         # Open video stream with OpenCV
         capture = cv2.VideoCapture(stream_url)
         if capture.isOpened():
+            print("OPened")
             prev_frame = None
-            end, frame = capture.read()
-            while not end:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                if prev_frame:
-                    difference = cv2.absdiff(frame, prev_frame)
+            success, frame = capture.read()
+            print("END: ", success)
+            while success:
+                gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                if prev_frame is not None:
+                    difference = cv2.absdiff(gray_frame, prev_frame)
                     mean_diff = np.mean(difference)
-                    
+                    if mean_diff > threshold:
+                        frame_num = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
+                        cv2.imwrite(f"frame_{frame_num}.jpg", gray_frame)  # Save key frame as image
+                        print(f"Saved key frame: frame_{frame_num}.jpg")
+                        cv2.imshow("Key Moment Frame", frame)
+                        if cv2.waitKey(0) & 0xFF == ord('q'):
+                            break
+
+                prev_frame = gray_frame
+                success, frame = capture.read()
+
+        else:
+            print("couldn't open")
+
+        capture.release()
+        cv2.destroyAllWindows()
+
+
+KeyMoments.detect_key_moments()
 
 
 
